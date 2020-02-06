b840ded8362c097c6e7a125b60ee2ae5102e4c1a
Donquixote Doflamingo <vikaspandeyxx@gmail.com>
2020-02-06 22:31:14 +0530

    Banner done

---
+++ DummyWebsite/Banners_Templates .cs
@@ -0,0 +1,32 @@
+﻿
+
+
+
+
+
+
+
+// ReSharper disable InconsistentNaming
+namespace DummyWebsite
+{
+    public static partial class Templates
+    {
+        #region /sitecore/templates/Sitecore Demo/Banners/Banner
+        /// <summary>
+        ///   Banner
+        ///   <para>ID: {004FD93D-EB70-46CA-9867-27D24DE23045}</para>
+        ///   <para>Path: /sitecore/templates/Sitecore Demo/Banners/Banner</para>
+        /// </summary>
+        public static class Banner
+        {
+            public static readonly Sitecore.Data.TemplateID ID = new Sitecore.Data.TemplateID(new Sitecore.Data.ID("{004FD93D-EB70-46CA-9867-27D24DE23045}"));
+            public const string IdString = "{004FD93D-EB70-46CA-9867-27D24DE23045}";
+
+        }
+        #endregion
+    }
+}
+
+
+
+
