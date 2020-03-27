4979bc814d5b4f5c7fd9907ec01920ebc8cd5b9e
Donquixote Doflamingo <vikaspandeyxx@gmail.com>
2020-03-27 15:21:20 +0530

    new changes completed

---
+++ DummyWebsite/Controllers/ServicesController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Sitecore;
+using Sitecore.Mvc;
+using Sitecore.Mvc.Presentation;
+using Sitecore.Data;
+using DummyWebsite;
+using Sitecore.Data.Items;
+
+namespace DummyWebsite.Controllers
+{
+    public class ServicesController : Controller
+    {
+        // GET: Services
+        public ActionResult GetServices()
+        {
+            Database currentDb = Sitecore.Context.Database;
+
+            string datasource = RenderingContext.Current.Rendering.DataSource;
+
+            Item item = currentDb.GetItem(datasource);
+
+            List<Item> itemServices = item.GetChildren().ToList();
+
+            return View("/Views/ServicesSection.cshtml", itemServices);
+        }
+    }
+}
---
+++ DummyWebsite/Services_Templates.cs
@@ -0,0 +1,147 @@
+﻿
+
+
+
+
+
+
+
+// ReSharper disable InconsistentNaming
+namespace DummyWebsite
+{
+    public static partial class Templates
+    {
+        #region /sitecore/templates/Sitecore Demo/Services/Files
+        /// <summary>
+        ///   Files
+        ///   <para>ID: {66F54BDA-9C21-4878-B4DD-967FEB02322B}</para>
+        ///   <para>Path: /sitecore/templates/Sitecore Demo/Services/Files</para>
+        /// </summary>
+        public static class Files
+        {
+            public static readonly Sitecore.Data.TemplateID ID = new Sitecore.Data.TemplateID(new Sitecore.Data.ID("{66F54BDA-9C21-4878-B4DD-967FEB02322B}"));
+            public const string IdString = "{66F54BDA-9C21-4878-B4DD-967FEB02322B}";
+
+            public static class Fields
+            {
+                /// <summary>
+                ///   File Icon
+                ///   <para>{8B9D3ED6-7639-4D77-8A7B-5C41E3941892}</para>
+                /// </summary>
+                public static readonly Sitecore.Data.ID FileIcon = new Sitecore.Data.ID("{8B9D3ED6-7639-4D77-8A7B-5C41E3941892}");
+
+                /// <summary>
+                ///   File Icon
+                ///   <para>{8B9D3ED6-7639-4D77-8A7B-5C41E3941892}</para>
+                /// </summary>
+                public const string FileIcon_FieldName = "File Icon";
+
+                /// <summary>
+                ///   File Link
+                ///   <para>{B08B07EE-8DFA-42F0-8E25-137721A52926}</para>
+                /// </summary>
+                public static readonly Sitecore.Data.ID FileLink = new Sitecore.Data.ID("{B08B07EE-8DFA-42F0-8E25-137721A52926}");
+
+                /// <summary>
+                ///   File Link
+                ///   <para>{B08B07EE-8DFA-42F0-8E25-137721A52926}</para>
+                /// </summary>
+                public const string FileLink_FieldName = "File Link";
+
+                /// <summary>
+                ///   FIle Summary
+                ///   <para>{ACA06E15-B9CE-4725-A521-F5F30A69A8F4}</para>
+                /// </summary>
+                public static readonly Sitecore.Data.ID FIleSummary = new Sitecore.Data.ID("{ACA06E15-B9CE-4725-A521-F5F30A69A8F4}");
+
+                /// <summary>
+                ///   FIle Summary
+                ///   <para>{ACA06E15-B9CE-4725-A521-F5F30A69A8F4}</para>
+                /// </summary>
+                public const string FIleSummary_FieldName = "FIle Summary";
+
+                /// <summary>
+                ///   File Title
+                ///   <para>{BDF8CACB-91F4-4DAB-8B02-63A7E386EFA9}</para>
+                /// </summary>
+                public static readonly Sitecore.Data.ID FileTitle = new Sitecore.Data.ID("{BDF8CACB-91F4-4DAB-8B02-63A7E386EFA9}");
+
+                /// <summary>
+                ///   File Title
+                ///   <para>{BDF8CACB-91F4-4DAB-8B02-63A7E386EFA9}</para>
+                /// </summary>
+                public const string FileTitle_FieldName = "File Title";
+
+            }
+        }
+        #endregion
+        #region /sitecore/templates/Sitecore Demo/Services/Service item
+        /// <summary>
+        ///   Service item
+        ///   <para>ID: {048BBBAB-25CC-4103-B72D-A7B00C3A5658}</para>
+        ///   <para>Path: /sitecore/templates/Sitecore Demo/Services/Service item</para>
+        /// </summary>
+        public static class ServiceItem
+        {
+            public static readonly Sitecore.Data.TemplateID ID = new Sitecore.Data.TemplateID(new Sitecore.Data.ID("{048BBBAB-25CC-4103-B72D-A7B00C3A5658}"));
+            public const string IdString = "{048BBBAB-25CC-4103-B72D-A7B00C3A5658}";
+
+            public static class Fields
+            {
+                /// <summary>
+                ///   Description
+                ///   <para>{B546DBD8-3CBC-4588-8AF6-D9BE0872E08C}</para>
+                /// </summary>
+                public static readonly Sitecore.Data.ID Description = new Sitecore.Data.ID("{B546DBD8-3CBC-4588-8AF6-D9BE0872E08C}");
+
+                /// <summary>
+                ///   Description
+                ///   <para>{B546DBD8-3CBC-4588-8AF6-D9BE0872E08C}</para>
+                /// </summary>
+                public const string Description_FieldName = "Description";
+
+                /// <summary>
+                ///   Service Summary
+                ///   <para>{A4D8B31C-2007-45EB-8C20-33C6126A89D1}</para>
+                /// </summary>
+                public static readonly Sitecore.Data.ID ServiceSummary = new Sitecore.Data.ID("{A4D8B31C-2007-45EB-8C20-33C6126A89D1}");
+
+                /// <summary>
+                ///   Service Summary
+                ///   <para>{A4D8B31C-2007-45EB-8C20-33C6126A89D1}</para>
+                /// </summary>
+                public const string ServiceSummary_FieldName = "Service Summary";
+
+                /// <summary>
+                ///   Service Title
+                ///   <para>{EDF35C80-A4F6-4FD2-A2A6-E601F8191A22}</para>
+                /// </summary>
+                public static readonly Sitecore.Data.ID ServiceTitle = new Sitecore.Data.ID("{EDF35C80-A4F6-4FD2-A2A6-E601F8191A22}");
+
+                /// <summary>
+                ///   Service Title
+                ///   <para>{EDF35C80-A4F6-4FD2-A2A6-E601F8191A22}</para>
+                /// </summary>
+                public const string ServiceTitle_FieldName = "Service Title";
+
+                /// <summary>
+                ///   Thumbnail Image
+                ///   <para>{1B491ECB-4EF0-4C35-A23A-AA8AB981AD87}</para>
+                /// </summary>
+                public static readonly Sitecore.Data.ID ThumbnailImage = new Sitecore.Data.ID("{1B491ECB-4EF0-4C35-A23A-AA8AB981AD87}");
+
+                /// <summary>
+                ///   Thumbnail Image
+                ///   <para>{1B491ECB-4EF0-4C35-A23A-AA8AB981AD87}</para>
+                /// </summary>
+                public const string ThumbnailImage_FieldName = "Thumbnail Image";
+
+            }
+        }
+        #endregion
+    }
+}
+
+
+
+
